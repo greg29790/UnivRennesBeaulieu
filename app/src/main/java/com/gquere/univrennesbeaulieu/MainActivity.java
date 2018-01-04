5346b32aa1f10a89f80ea8aa43a0d19aad594357
Quere Gregory <quere.gregory@gmail.com>
2018-01-04 18:05:08 +0100

    - Create first menu

--- app/src/main/java/com/gquere/univrennesbeaulieu/MainActivity.java
+++ app/src/main/java/com/gquere/univrennesbeaulieu/MainActivity.java
@@ -2,6 +2,8 @@ package com.gquere.univrennesbeaulieu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle(R.string.ActionBarTitle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.all :
+                /*SHOW ALL MAP*/
+                return true;
+            case R.id.rennes :
+                /*Show subMenu with rennes 1 building list*/
+                return true;
+            case R.id.iut :
+                /*SHOW subMenu with iut building list*/
+                return true;
+            case R.id.parking :
+                /*SHOW subMenu with parking list*/
+                return true;
+            case R.id.other :
+                /*SHOW subMenu with other building list*/
+            return true;
+        }
+        return (super.onOptionsItemSelected(item));
     }
 }
